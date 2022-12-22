4c229dd9f8f1f5d58920327d39e5fcc61c17d412
SC2309 <121182370+sc2309@users.noreply.github.com>
2022-12-22 15:30:27 +0530

    Update trial.cpp

--- trial.cpp
+++ trial.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 
 int main(){
-    string hobbies = "Coding,Cycling,Football", learning = "Driving Activa,C++,etc."
+    string hobbies = "Coding,Cycling,Football", learning = "Driving Activa,C++,etc.";
       cout << "I am Sarthak Chouksey and my hobbies are " << hobbies << endl << "I am learning many things like " << learning << endl << "Thankyou";
   return 0;
 }
