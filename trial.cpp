c8383ab1d470016f874dc2ec1f937b782d678612
SC2309 <121182370+sc2309@users.noreply.github.com>
2023-01-25 21:54:19 +0530

    Delete trial.cpp

--- trial.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-
-
-int main(){
-    string hobbies = "Coding,Cycling,Football", learning = "Driving Activa,C++,etc.";
-      cout << "I am Sarthak Chouksey and my hobbies are " << hobbies << endl << "I am learning many things like " << learning << endl << "Thankyou";
-  return 0;
-}
