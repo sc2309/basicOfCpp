08bc1f89e48dbb367bd43cda9d5902aad1130ca6
SC2309 <121182370+sc2309@users.noreply.github.com>
2023-01-25 20:49:39 +0530

    Create sample.cpp

---
+++ cpp/sample.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+
+int main(){
+  int n = 5;
+  string name = "sarthak";
+  for(int i = 0;i<n;++i){
+       cout << name[i] << endl;
+  }
+  return 0;
+}
