7f445ccce6ae1e1e0df474dbd1ced6a81cc8dfb5
SC2309 <121182370+sc2309@users.noreply.github.com>
2023-01-29 22:56:57 +0530

    Create exAlias.cpp

---
+++ myProjects/exAlias.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+
+int main(){
+	
+	using vegetables = string;
+	vegetables v1 = "tomato";
+	vegetables v2 = "potato";
+	vegetables v3 = "dhaniya";
+	cout << "Item one is " << v1 << endl << "Item two is " << v2 << endl << "Item three is " << v3 << endl;
+	 
+	return 0;
+}
