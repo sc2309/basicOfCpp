1d8e0eefc51aa40547b40a2da7b33a86cc9a8fb8
SC2309 <121182370+sc2309@users.noreply.github.com>
2023-01-29 22:47:00 +0530

    Add files via upload

---
+++ cpp/exAlias.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+
+int main(){
+	
+	using vegetables = string;
+	vegetables v1 = "tomato";
+	vegetables v2 = "potato";
+	vegetables v3 = "dhaniya";
+	cout << v1 << endl << v2 << endl << v3 << endl;
+	 
+	return 0;
+}
