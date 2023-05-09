f7b20463c60ebbb338f7b13cf9849cddd0829bc9
Sarthak <sc2309>
2023-05-09 16:44:34 +0530

    compiler testing succesfull

---
+++ cpp/name.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+
+int main(){
+    string name;
+    cout << "hello user pleae enter your full name:-" << endl;
+    getline(cin,name);
+    cout << "Good Morning " << name;
+
+    return 0;
+}
