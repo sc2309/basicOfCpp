e57f1c001c6f5fe4787eba7bfc66f240ad2ad3f5
Sarthak <sc2309>
2023-05-15 10:15:26 +0530

    solved structure problem

---
+++ Hacker_Rank/structP.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+struct Student{
+    int age, standard;
+    string first_name, last_name;
+};
+
+int main() {
+    Student st;
+    cin >> st.age;
+    cin >> st.first_name;
+    cin >> st.last_name;
+    cin >> st.standard;
+    for(int i=0;i<=70;i++){
+        cout << "-";
+    }
+    cout << endl;
+    cout << st.age << " " << st.first_name << " " << st.last_name << " " << st.standard;
+    
+    return 0;
+}
