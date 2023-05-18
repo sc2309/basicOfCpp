e9c27f47bd092e44698367a082306432f4796e04
Sarthak <sc2309>
2023-05-18 22:14:34 +0530

    bug on obstacles not creating

--- myProjects/drunk_man_race.cpp
+++ myProjects/drunk_man_race.cpp
@@ -8,12 +8,14 @@ using namespace std;
 class Drunk_man{
     private:
         string diff,choise,enter;                  //   here 'diff' is difficulty
-        int randSteps,target,rand_Obs,obsCount,left,age,Obs_choise;   // here 't' is the target and rand_Obs is a random obstacle  
+        int randSteps,target,rand_Obs,obsCount,left,age,Obs_choise,obsTime;   // here 't' is the target and rand_Obs is a random obstacle  
         string ins = "Please press a\n";
-        string help = "\n\n\nWe will generate a random number which will be your steps >=6\nWe will give you a target and you have to achieve this\nAnd there will be some obstacles \nEg. go left or right in one of these there are bombs\nIf you go in that path you will lose if you go in correct you will win\nPress a to continue\n\n";
+        string help = "\n\n\nWe will generate a random number which will be your steps >=6\nWe will give you a target and you have to achieve this\nAnd there will be some obstacles \nEg. go left or right in one of these there are bombs\nIf you go in that path you will lose if you go in correct you will be safe and have to complete\nthe target\n\n";
     public:
         int start(){
             cout << help;
+            cout << ins;
+            cin >> enter;
             cout << "Please enter your age:-\n\n";
             cin >> age;
             if(age >= 7){
@@ -26,7 +28,7 @@ class Drunk_man{
         }
         int DifficultySelector(){
             cout << "Hello user please select the difficulty:-\neasy\nmedium\nhard\n\n";
-            cin >> choise;
+            cin >> diff;
             if(diff == "easy"){
                 showTarget();
                 cout << ins;
@@ -75,31 +77,60 @@ class Drunk_man{
                 randSteps = (rand() % (6 - 1 + 1) + 1);
                 cout << randSteps << " steps\n\n";
                 target = target - randSteps;
-                cout << "current target is "<< target << "\n\nplease press a";
+                cout << "current target is "<< target << "\n\nplease press a\n";
                 cin >> enter;
-                if(target == 0){
+                cout << obsTime << endl;
+                if(randSteps == obsTime){
+                    obstacle1();
+                }
+                if(target == 0 || target < 0){
                     cout << "\n\n--- YOU WIN ---\n\n";
-                    break;
+                    DifficultySelector();
                 }
             }
 
             return 0;
         }
-        int obstacle(){
+        int obstacle1(){
             if(diff == "easy"){
                 obsCount = 1;
-                rand_Obs = (rand() % (2 - 1 + 1)) + 1;
-                cout << "\n\nplease choose 1 for left and 2 for right\n\n";
-                cin >> Obs_choise;
-                if(Obs_choise == rand_Obs){
-                    stepGenerater();
-                }
-                else{
-                    cout << "you lose\n";
-                }
+                obstacle2();
             }
             else if(diff == "medium"){
+                obsCount = 2;
+                obstacle2();
+            }
+            else if(diff == "hard"){
+                obsCount = 3;
+                obstacle2();
+            }
+            if(diff == "easy"){
+                obsTime = rand() % target;
+            }
+            else if(diff == "medium"){
+                obsTime = rand() % target;
+            }
+            else if(diff == "hard"){
+                obsTime = rand() % target;
+            }
+            stepGenerater();
 
+            return 0;
+        }
+        int obstacle2(){
+            rand_Obs = rand() % 1;
+            cout << "\n\nplease choose 0 for left and 1 for right\n\n";
+            for(int i=obsCount;i<=0;i--){
+                cin >> Obs_choise;
+                obsCount = obsCount - 1;
+                if(Obs_choise == rand_Obs && obsCount == 0){
+                    stepGenerater();
+                    break;
+                }
+                else{
+                    cout << "--- BOOOM! --- \nyou lose\n";
+                    break;
+                }
             }
 
             return 0;
