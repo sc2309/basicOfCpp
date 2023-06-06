8a4f10a7b5774a2cdc78a66cb7fd27b4065cfbcf
Sarthak <sc2309>
2023-06-06 17:57:10 +0530

    started a project of BAYMAX

---
+++ myProjects/Baymax.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <ctime>
+using namespace std;
+
+class Robot{
+    protected:
+        string query,ans,username;
+    public:
+        int intro(){
+            cout << "Hello i am Baymax. Your personal talk assistant please tell your name\n";
+            getline(cin,username);
+            cout << "Hello " << username;
+            return 0;
+        }
+        int queryInput(){
+            for(int i=0;i<=1000000;i++){
+                getline(cin,query);
+                if(query == "what is the time" || query == "what's the time" || query == "tell me time"){
+                    time_t currentTime = time(0);
+                    char* timeString = ctime(&currentTime);
+                    cout << "Current time is: " << timeString << std::endl;
+                }
+                else if(query == ""){
+
+                }
+            }
+        }
+        
+};
+
+int main(){
+
+    return 0;
+}
