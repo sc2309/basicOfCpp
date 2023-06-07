91158768805bf31e9526c04e8d80621165345cc1
Sarthak <sc2309>
2023-06-07 21:49:08 +0530

    Handled an exception to check valid name

--- ErrorHandlingPractise/practise2.cpp
+++ ErrorHandlingPractise/practise2.cpp
@@ -6,8 +6,8 @@ class Myclass{
         int ValidCheck(string name){
             bool x = true;
             char c = name[0];
-            if(c == 'F' || c == 'W'){
-                x = false;
+            if(c == 'F' || c == 'W' || c == 'f' || c == 'w'){
+                x = false;               //        if name starts with F or f or W or w itwill show invalid
             }
             return x;
         }
@@ -15,17 +15,17 @@ class Myclass{
             try{
                 bool check = ValidCheck(name);
                 if(name.size() <= 2){
-                    cout << "Too Short: " << name.size() << endl;
+                    cout << "Too Short: " << name.size() << endl;        //   if name length is less than 2
                 }
                 else if(check == false){
-                    cout << "Invalid Name\n";
+                    cout << "Invalid Name\n";    //    prints invalid
                 }
                 else{
-                    throw(name);
+                    throw(name);   
                 }
             }
             catch(string n){
-                cout << "VALID!\n";
+                cout << "VALID!\n";        //    prints valid when a name passes all the test
             }
             return 0;
         }
